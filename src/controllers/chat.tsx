87d0f6cd5185d700f1105b19f6287baa55793a31
jacob dunning <jakeydunning98@gmail.com>
2026-01-03 21:33:36 -0700

    web sockets working

--- src/controllers/chat.tsx
+++ src/controllers/chat.tsx
@@ -2,24 +2,50 @@ import { Hono } from "hono";
 import { jsxRenderer } from "hono/jsx-renderer";
 import { ChatPage } from "../views/pages/chat-page";
 import { validateSession } from "../models/services/session.service";
+import { upgradeWebSocket, websocket } from "hono/bun";
+import { createMiddleware } from "hono/factory";
+import type { ServerWebSocket } from "bun";
 
 const app = new Hono();
 
-app.use(jsxRenderer());
-app.get("/chat", async (c) => {
+const validSessionMiddleware = createMiddleware(async (c, next) => {
   const isValidSession = await validateSession(c);
   if (!isValidSession) {
+    console.log("hit session middleware, no valid session");
     return c.redirect("/auth/sign-in");
   }
+  await next();
+});
+
+app.use(jsxRenderer());
+app.use("/chat/*", validSessionMiddleware);
+app.get("/chat", async (c) => {
   return c.render(<ChatPage chatSessionId={1} />);
 });
 app.get("/chat/:id", async (c) => {
-  const isValidSession = await validateSession(c);
-  const chatId: number = Number(c.req.param().id);
-  if (!isValidSession) {
-    return c.redirect("/auth/sign-in");
-  }
+  const chatId = Number(c.req.param("id"));
   return c.render(<ChatPage chatSessionId={chatId} />);
 });
+app.get(
+  "/chat-ws",
+  upgradeWebSocket(async (c) => {
+    return {
+      onOpen(evt, ws) {
+        const rawWs = ws.raw as ServerWebSocket;
+        rawWs.subscribe("chat-room");
+        console.log("opening ws connection from", ws.url?.hostname);
+      },
+      onMessage(evt, ws) {
+        console.log("event data", evt.data);
+      },
+      onError(evt, ws) {
+        console.error("encountered error");
+      },
+      onClose(evt, ws) {
+        ws.close();
+      },
+    };
+  }),
+);
 
 export default app;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import authRoutes from "./controllers/auth";
 import chatRoutes from "./controllers/chat";
+import { upgradeWebSocket, websocket } from "hono/bun";
+import { html } from "hono/html";
+import type { ServerWebSocket } from "bun";
 
 const app = new Hono();
 app.use(logger());
 app.route("/", chatRoutes);
 app.route("/auth", authRoutes);
-export default app;
+
+export default {
+  fetch: app.fetch,
+  websocket,
+};
